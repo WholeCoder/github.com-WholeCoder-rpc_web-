2e1d1b3b4afe63d712d91a681ead5e90cee97211
Rube5 <ruben.pierich@gmail.com>
2023-04-14 16:32:22 -0400

    Added short description for root command.

--- WholeCoder/rpc_web/cmd/root.go
+++ WholeCoder/rpc_web/cmd/root.go
@@ -12,14 +12,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "rpc_web",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:     "rpc_web",
+	Short:   "THis app runs an rpc and web server to list hacker names and it also has a command line component that lists great qualities of merakey groupo members.",
+	Long:    ``,
 	Version: "0.1",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
